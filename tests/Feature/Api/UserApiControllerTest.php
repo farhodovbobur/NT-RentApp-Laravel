c5f571820079aa5a739f6841dbc8b37d527d5535
Bobur Farhodov <farhodovbobur@MacBook-Pro-Bobur.local>
2024-11-07 18:35:30 +0500

    UPDATE: update tests

--- tests/Feature/Api/UserApiControllerTest.php
+++ tests/Feature/Api/UserApiControllerTest.php
@@ -1,7 +1,20 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+namespace Tests\Feature\Api;
 
-    $response->assertStatus(200);
-});
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserApiControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
